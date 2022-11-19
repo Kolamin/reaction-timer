995511bdbd4f2edcda3f6a6babdd844d3a4b20b5
Kolamin <gofranich@gmail.com>
2022-11-19 14:37:19 +0600

    Build a Reaction Timer Game

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
-  <button @click="start">play</button>
-  <Block />
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
 import Block from "@/components/Block";
-
+import Results from "@/components/Results"
 export default {
   name: 'App',
   data() {
     return {
      isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
-  components: {Block},
+  components: {Block, Results},
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
-      console.log(this.delay)
+      this.showResults = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
     }
   }
 }
@@ -35,4 +43,19 @@ export default {
   color: #444;
   margin-top: 60px;
 }
+button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
